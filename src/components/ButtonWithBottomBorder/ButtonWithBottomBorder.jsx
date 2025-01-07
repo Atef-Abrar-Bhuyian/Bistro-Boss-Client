48c52c87e102342c11ff21e5b4d76c6b19ebb388
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-07 19:52:13 +0600

    featured section added to home page

--- src/components/ButtonWithBottomBorder/ButtonWithBottomBorder.jsx
+++ src/components/ButtonWithBottomBorder/ButtonWithBottomBorder.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ButtonWithBottomBorder = ({content}) => {
     return (
         <div>
-            <button className="btn border-t-0 border-l-0 border-r-0 border-b-2 border-white hover:border-b-yellow-500 uppercase">{content}</button>
+            <button className="btn bg-transparent border-0 border-b-2 border-b-white hover:border-b-yellow-500 uppercase">{content}</button>
         </div>
     );
 };
