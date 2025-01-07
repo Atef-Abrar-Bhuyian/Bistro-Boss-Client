0c93407bd798d11af78e316eda7941fcbddc2352
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-07 22:48:30 +0600

    contact section added to home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -6,6 +6,7 @@ import Featured from "../Featured/Featured";
 import Testimonial from "../Testimonial/Testimonial";
 import ReactHelmet from "../../../components/ReactHelmet/ReactHelmet";
 import BistroSection from "../BistroSection/BistroSection";
+import ContactSection from "../ContactSection/ContactSection";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Category></Category>
       <BistroSection></BistroSection>
       <PopularMenu></PopularMenu>
+      <ContactSection></ContactSection>
       <Featured></Featured>
       <Testimonial></Testimonial>
     </div>
