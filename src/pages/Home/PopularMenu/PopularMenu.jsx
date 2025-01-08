c0574ee8024ad53dcadafbe5b42219eb278a5c18
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-08 18:22:08 +0600

    useMenu custom hooks created

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 import ButtonWithBottomBorder from "../../../components/ButtonWithBottomBorder/ButtonWithBottomBorder";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popularItems = menu.filter((item) => item.category === "popular");
 
   return (
     <section className="mb-12">
@@ -20,13 +13,15 @@ const PopularMenu = () => {
         subHeading={"Popular Items"}
         heading={"FROM OUR MENU"}
       ></SectionTitle>
-      <div  className="grid md:grid-cols-2 gap-8">
-      {menu.map((item) => (
-        <MenuItem key={item._id} item={item}></MenuItem>
-      ))}
+      <div className="grid md:grid-cols-2 gap-8">
+        {popularItems.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
       </div>
       <div className="flex items-center justify-center my-8">
-      <ButtonWithBottomBorder content={"View Full Menu"}></ButtonWithBottomBorder>
+        <ButtonWithBottomBorder
+          content={"View Full Menu"}
+        ></ButtonWithBottomBorder>
       </div>
     </section>
   );
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -10,8 +10,8 @@ const Cover = ({ img, title,description }) => {
       bgImageAlt="the dog"
       strength={-150}
     >
-      <div className="hero h-[500px] border-2">
-        <div className="hero-overlay bg-opacity-60 border-2"></div>
+      <div className="hero h-[500px]">
+        <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-white">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
